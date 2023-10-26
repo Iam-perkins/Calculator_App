0c313ddb163b11c303f37de18a05a5857d47a7f8
nji pejkins <njipekins@gmail.com>
2023-10-26 13:50:03 -0700

    made extra adjustments on the javascript

--- main.js
+++ main.js
@@ -2,28 +2,39 @@ const but9= document.getElementById("but9");
 let buttons= document.querySelectorAll("button");
 const clearButton = document.getElementById("otherbut9");
 let container = document.getElementById("first");
+const plus = document.getElementById("+");
 let buttonChoice ;
 
-but9.addEventListener("click",(e)=>{
+     buttons.forEach(buttons => buttons.addEventListener("click",(e)=>{
+        buttonChoice=e.target.id;
+        document.getElementById("first").innerHTML=" ";
+        container.innerHTML=buttonChoice;
+     }))
+
+/*but9.addEventListener("click",(e)=>{
     document.getElementById("first").innerHTML=" ";
     document.getElementById("first").innerHTML=9;
 
 })
-
+*/
 clearButton.addEventListener("click",(e)=>{
-    document.getElementById("first").innerHTML=" ";
-    document.getElementById("first").innerHTML=0;
+    document.getElementById("first").innerHTML=" enter";
+  
 
 })
 
-buttons.forEach(buttons=>{
+   plus.addEventListener("click",(e)=>{
     
-
-    buttons.addEventListener('click',(e)=>{
-    
-       e.target.innerHTML;
-       buttonChoice=e.target.innerHTML;
-       container= buttonChoice;
-
-    })
-})
+   })
+function add( a, b){
+    return a+ b;
+}
+function multiply(a, b){
+    return a*b;
+}
+function substract(a, b){
+    return a-b;
+}
+function divide(a, b){
+    return a/b;
+}
